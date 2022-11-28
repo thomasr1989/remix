0ef405ac1c434f2134ec88e962a0cae33679b83d
thomasr1989 <makepuchgreatagain@gmail.com>
2022-11-28 19:58:31 +0100

    update

--- contracts/AngryVeg.sol
+++ contracts/AngryVeg.sol
@@ -16,11 +16,10 @@ contract AngryVeg is ERC721("Angry Tomatoes","ANGRY TOMATOES"), Ownable{
         baseUri = _baseUri;
     }
 
-
     string private baseUri;
     bool private paused;
     uint private constant MAX_SUPPLY  = 5;
-    uint private  publicMintPrice = 0.01 ether;
+    uint private  publicMintPrice = 0.005 ether;
     
 
     function mintToken()  external payable mintOngoing{
@@ -28,41 +27,27 @@ contract AngryVeg is ERC721("Angry Tomatoes","ANGRY TOMATOES"), Ownable{
         require(balanceOf(msg.sender) == 0, "Max one Angry Tomaty per wallet.");
         _tokenIdCounter.increment();
         _safeMint(msg.sender,_tokenIdCounter.current());
-        // _safeMint(msg.sender, _tokenIdCounter.increment());
-        
-
-        // string memory uri = string(abi.encodePacked(tokenURI,Strings.toString(_tokenIdCounter.current())));
-    }
-
-    function nextMint() public view returns(uint nMint){
-        return _tokenIdCounter.current();
-    }
-
-
-    function setBaseUri(string calldata _baseUri) external onlyOwner{
-        baseUri = _baseUri;
-
     }
 
     function _baseURI() internal view override returns (string memory) {
         return baseUri;
     }
 
+    function withdraw(uint amount,address payable receiver) public onlyOwner{
+        require(amount < address(this).balance, "Insufficient Balance");
+        receiver.transfer(amount);
+    }
+
+    function setBaseUri(string calldata _baseUri) external onlyOwner{
+        baseUri = _baseUri;
+    }
+
     function setPause(bool _pause) external onlyOwner{
         paused = _pause;
     }
 
-     
 
-  
-
-    function withdraw(uint amount,address payable receiver) public onlyOwner{
-        require(amount < address(this).balance, "Insufficient Balance");
-        receiver.transfer(amount);
-
-    }
-
-    function updateMintPrice(uint amount) external onlyOwner {
+    function setMintPrice(uint amount) external onlyOwner {
         publicMintPrice = amount;
     }
 
@@ -70,7 +55,7 @@ contract AngryVeg is ERC721("Angry Tomatoes","ANGRY TOMATOES"), Ownable{
         return publicMintPrice;
     }
 
-      modifier mintOngoing(){
+    modifier mintOngoing(){
         require(_tokenIdCounter.current() < MAX_SUPPLY && !paused, "Mint paused / ended.");
         _;
     }
